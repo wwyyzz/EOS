aa07f24cdc3f5ff6748ec980164c86e50a83a794
wwyyzz <wwyyzz08@sina.com>
2016-12-13 17:13:23 +0800

    12-13 -1 1. modi function name 2. del module = 'NONE' row

--- eos-query.py
+++ eos-query.py
@@ -18,7 +18,6 @@ def get_eos_data():
     返回：eos_data字典数据
 
     """
-    eos_data = {}
     with open(r".\eos_data\eos-data", 'rb') as f:
         eos_data = pickle.load(f)
 
@@ -54,7 +53,7 @@ def get_device_type(version_info):
     return device_type
 
 
-def get_device_info(version_info, manu_info):
+def get_device_moudle(version_info, manu_info):
     """
     功能：获取设备信息信息
     通过正则，查找所有的DEVICE_NAME、DEVICE_SERIAL_NUMBER信息放入列表中
@@ -82,21 +81,23 @@ def get_device_info(version_info, manu_info):
     else:
         manu_info_list = [['unknown', '   unknown']]
 
-    device_info = [device_type, manu_info_list]
+    device_moudle = [device_type, manu_info_list]
     # print("device_info ============")
     # print(device_info)
-    return device_info
+    return device_moudle
 
 
-def get_summary_of_device(summary_list):
+def count_moudle(summary_list):
     """
     功能：获取板块的汇总统计信息
     将输入列表保存到数据库中，使用SQL进行数据汇总统计
     返回：基于型号、板卡的数量汇总
     """
 
-    #    conn = sqlite3.connect(r'D:\1-MY\2-Code\Python\EOS\device.db')
-    conn = sqlite3.connect(r'C:\MyCode\EOS\device.db')
+    # conn = sqlite3.connect(r'D:\1-MY\2-Code\Python\EOS\device.db')
+    # conn = sqlite3.connect(r'C:\MyCode\EOS\device.db')
+    conn = sqlite3.connect(r'.\device.db')
+    # C:\MyCode\EOS
     c = conn.cursor()
 
     c.execute("delete from DEVICE;")
@@ -106,12 +107,15 @@ def get_summary_of_device(summary_list):
 
     for device in summary_list:
         for num in range(len(device[1])):
-            c.execute("INSERT INTO DEVICE (device_type, module_type, module_sn, bom) VALUES (?, ?, ?, ?  )",
-                      [device[0], device[1][num][0], device[1][num][1], device[1][num][1][2:10]])
+            if device[1][num][0] != 'NONE':
+                c.execute("INSERT INTO DEVICE (device_type, module_type, module_sn, bom) VALUES (?, ?, ?, ?  )",
+                          [device[0], device[1][num][0], device[1][num][1], device[1][num][1][2:10]])
 
     conn.commit()
     c.execute(
-        "SELECT device_type, module_type, bom, COUNT(module_type) from DEVICE GROUP BY device_type, module_type ORDER BY device_type, module_type")
+        "SELECT device_type, module_type, bom, COUNT(module_type) from DEVICE "
+        "GROUP BY device_type, module_type "
+        "ORDER BY device_type, module_type")
     summary_of_device = c.fetchall()
     c.close()
 
@@ -119,7 +123,7 @@ def get_summary_of_device(summary_list):
     for moudle in summary_of_device:
         moudle_list = list(moudle)
         #    result_list.append((list)eos_data.get(moudle[2]))
-        eos_query = eos_data.get(moudle[2])
+        eos_query = eos_data_dict.get(moudle[2])
         if eos_query is not None:
             result = moudle_list + eos_query
         else:
@@ -130,7 +134,7 @@ def get_summary_of_device(summary_list):
     return result_list
 
 
-def get_devices_all(filename):
+def get_all_devices_moudle(file):
     """
     功能：获取无重复的设备型号、板卡型号和序列号sn
     读取指定文件,调用get_device_info 对 display version 和 display device manuinfo 信息进行解析，获取设备型号、版本型号和序列号sn
@@ -138,25 +142,26 @@ def get_devices_all(filename):
     返回：无重复数据的列表
     ['H3C MSR30-11', [['MSR 30-11', '210235A274B081000034'], ['RT-XMIM-24FSW', '210231A77BB081000272']]]
     """
-    devices_all = []
+    all_devices_moudle = []
 
-    book = xlrd.open_workbook(filename)
+    book = xlrd.open_workbook(file)
     sh = book.sheet_by_index(0)
     for i in range(1, sh.nrows - 1, 2):
-        device = get_device_info(sh.cell_value(i, 3), sh.cell_value(i + 1, 3))
-        devices_all.append(device)
+        device = get_device_moudle(sh.cell_value(i, 3), sh.cell_value(i + 1, 3))
+        all_devices_moudle.append(device)
 
-    devices_all.sort()
+    all_devices_moudle_duplicate_removal = []
+    for device in all_devices_moudle:
+        if device not in all_devices_moudle_duplicate_removal:
+            all_devices_moudle_duplicate_removal.append(device)
 
-    devices_all_duplicate_removal = []
-    for device in devices_all:
-        if device not in devices_all_duplicate_removal:
-            devices_all_duplicate_removal.append(device)
+    all_devices_moudle_duplicate_removal.sort()
 
-    return devices_all_duplicate_removal
+    print(all_devices_moudle_duplicate_removal)
+    return all_devices_moudle_duplicate_removal
 
 
-def write_xls(result, filename):
+def write_xls(result, file):
     """
     输入: 结果列表,保存文件名
     将汇总和查询结果写入到输出xls文件中
@@ -165,19 +170,29 @@ def write_xls(result, filename):
     book = xlwt.Workbook()
     sheet1 = book.add_sheet(u'sheet1', cell_overwrite_ok=True)
 
-    row0 = [u'型号', u'板卡类型', u'BOM编码', u'数量', u"产品线", u"所属PDT", u"EOM DCP实际时间", u"EOS DCP计划时间", u"EOS DCP实际时间",
-            u"EOS公告上网实际时间", u"EOS公告上网计划时间"]
+    # 写入表头数居
+    pattern = xlwt.Pattern()
+    pattern.pattern = xlwt.Pattern.SOLID_PATTERN
+    pattern.pattern_fore_colour = 5
+    style = xlwt.XFStyle()
+    style.pattern = pattern
+
+    row0 = [u'型号', u'板卡类型', u'BOM编码', u'数量',
+            u"产品线", u"所属PDT",
+            u"EOM DCP实际时间",
+            u"EOS DCP计划时间", u"EOS DCP实际时间", u"EOS公告上网实际时间", u"EOS公告上网计划时间"]
     for i in range(0, len(row0)):
         sheet1.col(i).width = 256 * 20
-        sheet1.write(0, i, row0[i])
+        sheet1.write(0, i, row0[i], style)
 
-    n = 1
+    # 数居
+    row_number = 1
     for line in result:
-        for i in range(0, 11):
-            sheet1.write(n, i, line[i])
-        n += 1
+        for col in range(0, 11):
+            sheet1.write(row_number, col, line[col])
+        row_number += 1
 
-    book.save(filename)
+    book.save(file)
 
 
 '''
@@ -187,15 +202,17 @@ def write_xls(result, filename):
 
 INPUT_DIR = r'.\\H3C-display\\'
 OUTPUT_DIR = r'.\\output\\'
-eos_data = get_eos_data()
+eos_data_dict = get_eos_data()
 
 # files = os.listdir(r'.\\H3C-display\\')
 files = os.listdir(r'.\\test-input\\')
 
+# 指定输入目录中的待处理文件,进行分析汇总,输出结果文件
 for filename in files:
     PATH = INPUT_DIR + filename
     output_filename = OUTPUT_DIR + filename.split('.')[0] + r'-summary.xls'
     print(r"保存文件名为:" + output_filename)
-    devices_all_duplicate_removal = get_devices_all(PATH)
-    summary_of_device = get_summary_of_device(devices_all_duplicate_removal)
-    write_xls(summary_of_device, output_filename)
+
+    all_devices_moudle_list = get_all_devices_moudle(PATH)
+    summary_of_moudle = count_moudle(all_devices_moudle_list)
+    write_xls(summary_of_moudle, output_filename)
